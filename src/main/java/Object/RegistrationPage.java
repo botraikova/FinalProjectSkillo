317814ad58a15a861305f0abf7755c9b87971a74
slayfen <147399701+slayfen@users.noreply.github.com>
2024-04-23 22:21:30 +0300

    final project for Automation Course

--- src/main/java/Object/RegistrationPage.java
+++ src/main/java/Object/RegistrationPage.java
@@ -1,4 +1,5 @@
 package Object;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,32 +20,43 @@ public class RegistrationPage {
         return wait.until(ExpectedConditions.urlToBe(REG_PAGE_URL));
     }
 
-    public void typeUserName(String username){
+    public void registerLinkButton() {
+        WebDriverWait wait = new WebDriverWait(this.webDriver, Duration.ofSeconds(15));
+        WebElement registrationLink = wait.until(ExpectedConditions.elementToBeClickable(
+                webDriver.findElement(By.xpath("//form//p//a[text()='Register']"))));
+        registrationLink.click();
+    }
+
+    public void typeUserName(){
+        String username = generateRandomUsername(10,15);
         WebDriverWait wait = new WebDriverWait(this.webDriver, Duration.ofSeconds(15));
         WebElement usernameTextField = wait.until(ExpectedConditions.visibilityOf(webDriver.
-                findElement(By.xpath("/html/body/app-root/div[2]/app-register/div/div/form/div[1]/input"))));
+                findElement(By.xpath("//*[@name='username']"))));
         usernameTextField.sendKeys(username);
     }
 
-    public void typeEmail(String email){
+    public void typeEmail(){
+        String email = generateRandomEmailAddress(10,15);
         WebDriverWait wait = new WebDriverWait(this.webDriver, Duration.ofSeconds(15));
         WebElement emailTextField = wait.until(ExpectedConditions.visibilityOf(webDriver.
-                findElement(By.xpath("/html/body/app-root/div[2]/app-register/div/div/form/div[2]/input"))));
+                findElement(By.xpath("//*[@type='email']"))));
         emailTextField.sendKeys(email);
     }
 
-    public void typePassword(String password){
+    String password = generateRandomAlphabeticString(10,15);
+
+    public void typePassword(){
         WebDriverWait wait = new WebDriverWait(this.webDriver, Duration.ofSeconds(15));
         WebElement passwordTextField = wait.until(ExpectedConditions.visibilityOf(webDriver.
                 findElement(By.xpath("//*[@id='defaultRegisterFormPassword']"))));
         passwordTextField.sendKeys(password);
     }
 
-    public void typeConfirmPassword(String confirmPassword){
+    public void typeConfirmPassword(){
         WebDriverWait wait = new WebDriverWait(this.webDriver, Duration.ofSeconds(15));
         WebElement passwordAgainTextField = wait.until(ExpectedConditions.visibilityOf(webDriver.
                 findElement(By.xpath("//*[@id='defaultRegisterPhonePassword']"))));
-        passwordAgainTextField.sendKeys(confirmPassword);
+        passwordAgainTextField.sendKeys(password);
     }
 
     public void clickSignIn(){
@@ -53,4 +65,17 @@ public class RegistrationPage {
                 (By.xpath("//*[@id='sign-in-button']"))));
         signInButton.click();
     }
+
+    private String generateRandomUsername(int minLengthInclusive, int maxLengthInclusive) {
+        return RandomStringUtils.randomAlphanumeric(minLengthInclusive, maxLengthInclusive);
+    }
+
+    private String generateRandomEmailAddress(int minLengthInclusive, int maxLengthInclusive) {
+        return generateRandomAlphabeticString(minLengthInclusive, maxLengthInclusive) + "@gmail.com";
+    }
+
+    private String generateRandomAlphabeticString(int minLengthInclusive, int maxLengthInclusive) {
+        return RandomStringUtils.randomAlphanumeric(minLengthInclusive, maxLengthInclusive);
+    }
+
 }
--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,26 +1,13 @@
 import Object.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import java.time.Duration;
 
 
 public class RegistrationTest extends DriverMaintenanceAndScreenshots {
 
 
-    @DataProvider(name = "getUser")
-    public Object[][] getUsers() {
-        return new Object[][]{
-                {"usertofailthetest", "hellogoodbye@mail.com", "1234567", "1234567"},
-        };
-    }
-
-    @Test (dataProvider = "getUser")
-    public void registrationTest (String username, String email, String password, String confirmPassword){
+    @Test
+    public void registrationTest (){
         HomePage homePage = new HomePage(webDriver);
         Header header = new Header(webDriver);
         LoginPage loginPage = new LoginPage(webDriver);
@@ -31,20 +18,15 @@ public class RegistrationTest extends DriverMaintenanceAndScreenshots {
         Assert.assertTrue(homePage.isUrlLoaded(), "Home page isn't loaded");
 
         header.clickLoginButton();
-        String loginURL = "http://training.skillo-bg.com:4200/users/login";
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(15));
-        wait.until(ExpectedConditions.urlToBe(loginURL));
+
         Assert.assertTrue(loginPage.isUrlLoaded(), "Login page isn't loaded");
 
-        wait = new WebDriverWait(this.webDriver, Duration.ofSeconds(15));
-        WebElement registerLinkButton = wait.until(ExpectedConditions.visibilityOf(webDriver.findElement
-                (By.xpath("/html/body/app-root/div[2]/app-login/div/div/form/p[2]/a"))));
-        registerLinkButton.click();
 
-        registrationPage.typeUserName(username);
-        registrationPage.typeEmail(email);
-        registrationPage.typePassword(password);
-        registrationPage.typeConfirmPassword(confirmPassword);
+        registrationPage.registerLinkButton();
+        registrationPage.typeUserName();
+        registrationPage.typeEmail();
+        registrationPage.typePassword();
+        registrationPage.typeConfirmPassword();
 
         registrationPage.clickSignIn();
 
